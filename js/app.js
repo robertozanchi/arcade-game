1fa834de0c5ebec67c064bff308d0554ead076d0
robertozanchi <r.zanchi@gmail.com>
2015-12-28 13:57:13 +0100

    Evened out indentation

--- js/app.js
+++ js/app.js
@@ -1,24 +1,24 @@
 // Enemies the player must avoid
 var Enemy = function(x, y) {
-  // Set the enemy image to bug
-  this.sprite = 'images/enemy-bug.png';
-  // Set the enemy's postion
-  this.x = x;
-  this.y = y;
-  // Set the enemy's initial speed
-  this.rate = 100 + Math.floor(Math.random() * 150);
+    // Set the enemy image to bug
+    this.sprite = 'images/enemy-bug.png';
+    // Set the enemy's postion
+    this.x = x;
+    this.y = y;
+    // Set the enemy's initial speed
+    this.rate = 100 + Math.floor(Math.random() * 150);
 };
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-  this.x = this.x + (dt * this.rate);
-  // When bug goes off one side, reappear on the other side
-  if (this.x > 700){
-    this.x = -100;
-    // Assigns a new speed to enemy
-    this.rate = 100 + Math.floor(Math.random() * 150);
-  }
+    this.x = this.x + (dt * this.rate);
+    // When bug goes off one side, reappear on the other side
+    if (this.x > 700){
+        this.x = -100;
+        // Assigns a new speed to enemy
+        this.rate = 100 + Math.floor(Math.random() * 150);
+    }
 };
 
 // Reset player when she collides with an enemy
@@ -38,55 +38,55 @@ Enemy.prototype.render = function() {
 
 // Player class
 var Player = function(x,y) {
-  // Set the player image to boy character
-  this.sprite = 'images/char-boy.png';
-  // Set the player's position
-  this.x = x;
-  this.y = y;
+    // Set the player image to boy character
+    this.sprite = 'images/char-boy.png';
+    // Set the player's position
+    this.x = x;
+    this.y = y;
 };
 
 Player.prototype.update = function() {
-  // When player reaches water, the position is reset
-  if (this.y < 0) {
-    player.reset();
+    // When player reaches water, the position is reset
+    if (this.y < 0) {
+        player.reset();
   }
 };
 
 // Resets player's position to initial position
 Player.prototype.reset = function () {
-  this.x = 303;
-  this.y = 404;
+    this.x = 303;
+    this.y = 404;
 };
 
 // Handle keyboard input during gameplay
 Player.prototype.handleInput = function(key) {
-  switch(key) {
-    case 'up':
-      if (this.y > 0){
-        this.y -= 83;
-      }
-      break;
-    case 'down':
-      if (this.y < 404) {
-        this.y += 83;
-      }
-      break;
-    case 'left':
-      if (this.x > 0) {
-        this.x -= 101;
-      }
-      break;
-    case 'right':
-      if (this.x < 404){
-        this.x += 101;
-      }
-      break;
-  }
+    switch(key) {
+        case 'up':
+        if (this.y > 0){
+            this.y -= 83;
+        }
+        break;
+        case 'down':
+        if (this.y < 404) {
+            this.y += 83;
+        }
+        break;
+        case 'left':
+        if (this.x > 0) {
+            this.x -= 101;
+        }
+        break;
+        case 'right':
+        if (this.x < 404){
+            this.x += 101;
+        }
+        break;
+    }
 };
 
 // Draw the player on the screen
 Player.prototype.render = function() {
-  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
 
 // Instantiate enemy objects and put them in an array called allEnemies
