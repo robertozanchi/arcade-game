35cdea0dd4223422aaeec60afbe7632aeae5316c
robertozanchi <r.zanchi@gmail.com>
2015-12-28 15:05:45 +0100

    Create Character superclass using pseudoclassical design

--- js/app.js
+++ js/app.js
@@ -1,13 +1,27 @@
-// Enemies the player must avoid
-var Enemy = function(x, y) {
-    // Set the enemy image to bug
-    this.sprite = 'images/enemy-bug.png';
-    // Set the enemy's postion
+// Character superclass for player and enemies
+var Character = function(x, y, image) {
+    // Set character's image
+    this.sprite = image;
+    // Set the character's position
     this.x = x;
     this.y = y;
+}
+
+// Draw the character on the screen, required method for game
+Character.prototype.render = function() {
+    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+};
+
+// Enemies the player must avoid
+var Enemy = function(x, y, image) {
+    Character.call(this, x, y, image);
     // Set the enemy's initial speed
     this.rate = 100 + Math.floor(Math.random() * 150);
 };
+
+// Set prototype and prototype.constructor for inheritance
+Enemy.prototype = Object.create(Character.prototype);
+Enemy.prototype.constructor = Character;
 
 // Update the enemy's position, required method for game
 // Parameter: dt, a time delta between ticks
@@ -31,19 +45,14 @@ Enemy.prototype.checkCollision = function() {
     }
 };
 
-// Draw the enemy on the screen, required method for game
-Enemy.prototype.render = function() {
-    ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+// Player class
+var Player = function(x, y, image) {
+    Character.call(this, x, y, image);
 };
 
-// Player class
-var Player = function(x,y) {
-    // Set the player image to boy character
-    this.sprite = 'images/char-boy.png';
-    // Set the player's position
-    this.x = x;
-    this.y = y;
-};
+// Set prototype and prototype.constructor for inheritance
+Player.prototype = Object.create(Character.prototype);
+Player.prototype.constructor = Character; 
 
 Player.prototype.update = function() {
     // When player reaches water, the position is reset
@@ -92,9 +101,9 @@ Player.prototype.render = function() {
 };
 
 // Instantiate enemy objects and put them in an array called allEnemies
-var enemy1 = new Enemy(-101, 55);
-var enemy2 = new Enemy(-101, 140);
-var enemy3 = new Enemy(-101, 225);
+var enemy1 = new Enemy(-101, 55, 'images/enemy-bug.png');
+var enemy2 = new Enemy(-101, 140, 'images/enemy-bug.png');
+var enemy3 = new Enemy(-101, 225, 'images/enemy-bug.png');
 
 var allEnemies = [];
 allEnemies.push(enemy1);
@@ -102,7 +111,7 @@ allEnemies.push(enemy2);
 allEnemies.push(enemy3);
 
 // Place the player object in a variable called player
-var player = new Player(303, 404);
+var player = new Player(303, 404, 'images/char-boy.png');
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
